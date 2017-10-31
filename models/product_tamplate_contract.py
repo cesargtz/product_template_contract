77e057258fed0b1cd900e3f3e3210f2e40b64f73
César Gtz <cesareomargtz@gmail.com>
2017-10-31 09:40:25 -0700

    fix error field

--- models/product_tamplate_contract.py
+++ models/product_tamplate_contract.py
@@ -4,4 +4,4 @@ from odoo import models, fields, api
 
 class product_template_contract(models.Model):
     _inherit = 'product.template'
-    consider_contract = fields.Boolean(help="Suma el total de toneladas en los contratos" string="Considerar para contrato")
+    consider_contract = fields.Boolean(help="Suma el total de toneladas en los contratos", string="Considerar para contrato")
